7c6311e325033fd2642ecc2f2f4470175ee8edd9
alogx11 <alogx11@gmail.com>
2023-11-05 19:02:10 -0600

    added blue tank

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
-let ball;
-let tankRed;
+let redTank, blueTank;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  ball = new Sprite();
-  ball.diameter = 50;
-  redTank = new Tank(200, 200, windowWidth * 0.04, color(255, 0, 0), 2);
+  // tank param (x, y, width, color, speed)
+  redTank = new Tank(
+    windowWidth / 4,
+    windowHeight / 2,
+    windowWidth * 0.04,
+    color(255, 0, 0),
+    2
+  );
+  blueTank = new Tank(
+    (windowWidth / 4) * 3,
+    windowHeight / 2,
+    windowWidth * 0.04,
+    color(0, 0, 255),
+    2
+  );
 }
 
 function draw() {
   background("gray");
-  // redTank.display();
+  // Handle controls for the red tank (wasd)
   if (keyIsDown(65) || keyIsDown(97)) {
     // 65 is the key code for "a", and 97 is the key code for "A"
     redTank.turnLeft();
@@ -27,17 +38,24 @@ function draw() {
     // 83 is the key code for "s", and 115 is the key code for "S"
     redTank.moveBack();
   }
+
+  // Handle controls for the blue tank (Arrow keys)
+  if (keyIsDown(LEFT)) {
+    blueTank.turnLeft();
+  }
+  if (keyIsDown(RIGHT)) {
+    blueTank.turnRight();
+  }
+  if (keyIsDown(UP)) {
+    blueTank.moveForward();
+  }
+  if (keyIsDown(DOWN)) {
+    blueTank.moveBack();
+  }
   redTank.display();
+  blueTank.display();
 }
 
 function keyPressed() {
   print(keyCode);
-  // if (key === "a" || key === "A") {
-  //   this.turnLeft();
-  // } else if (key === "d" || key === "D") {
-  //   this.turnRight();
-  // } else if (key === "w" || key === "W") {
-  //   this.moveForward();
-  // } else if (key === "s" || key === "S") {
-  //   this.moveBack();
 }
