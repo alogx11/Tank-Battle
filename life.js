9c626171f7710ffa5708dac8a3112ed67595c5c4
alogx11 <alogx11@gmail.com>
2023-11-25 19:37:13 -0600

    created contuctor for life

--- life.js
+++ life.js
@@ -1,1 +1,11 @@
-class GameOfLife {}
+class GameOfLife {
+  constructor(cellSize) {
+    this.cellSize = cellSize;
+    this.grid = [];
+    this.nextGrid = [];
+    this.bgColor = color(255);
+    this.aliveColor = color(0);
+    this.columns = Math.floor(windowWidth / cellSize);
+    this.rows = Math.floor(windowHeight / cellSize);
+  }
+}
